f9de5acdb512181900fdcf62805e5eceb594ae2c
bilasa <bilasa999@gmail.com>
2019-03-19 14:08:37 -0400

    implement method to return two t-test sample groups

--- single_task_predictor.py
+++ single_task_predictor.py
@@ -8,22 +8,48 @@ from sklearn.datasets import make_regression
 
 ################# SINGLE TASK DRUG RESPONSE PREDICTOR ################# 
 
+# Takes two numpy arrays as input: y_actual, y_predicted
+# Returns two sample groups:
+#   The first contains a list of predicted drug response values for which the patient's actual response was 0
+#   The second contains a list of predicted drug response values for which the patient's actual response was 1
+def get_t_test_groups(y_actual, y_predicted):
+    
+    # Ensure the sizes are the same
+    try:
+        assert len(y_actual) == len(y_predicted)
+    except AssertionError:
+        print("AssertionError: size of y_actual and y_predicted must be the same")
+
+    # Construct the two sample groups
+    drug_responses_0 = []
+    drug_responses_1 = []
+    for i in range(len(y_actual)):
+        if y_actual[i] == 0:
+            print("0 found!")
+            drug_responses_0.append(y_predicted[i])
+        elif y_actual[i] == 1:
+            print("1 found!")
+            drug_responses_1.append(y_predicted[i])
+
+    return drug_responses_0, drug_responses_1
+
+# Path to datasets
 data_path = '../Data/'
 
 # Load training and test set
 x_train = pd.read_csv(data_path + 'gdsc_expr_postCB.csv', index_col=0, header=None).T.set_index('cell line id')
 y_train = pd.read_csv(data_path + 'gdsc_dr_lnIC50.csv', index_col=0, header=None).T.set_index('cell line id')
 x_test = pd.read_csv(data_path + 'tcga_expr_postCB.csv', index_col=0, header=None).T.set_index('patient id')
-#y_test = pd.read_csv(data_path + 'tcga_dr.csv', index_col=0, header=None).T.set_index('patient id')
+y_test = pd.read_csv(data_path + 'tcga_dr.csv', index_col=0, header=None).T.set_index('patient id')
 
 # Normalize data for mean 0 and standard deviation of 1
 ss = StandardScaler()
 x_train = pd.DataFrame(ss.fit_transform(x_train), index = x_train.index, columns = x_train.columns)
 y_train = pd.DataFrame(ss.fit_transform(y_train), index = y_train.index, columns = y_train.columns)
-#x_test = pd.DataFrame(ss.fit_transform(x_test), index = x_test.index, columns = x_test.columns)
+x_test = pd.DataFrame(ss.fit_transform(x_test), index = x_test.index, columns = x_test.columns)
 
 # Replace NaN values by the mean
-x_train.fillna(x_train.mean(), inplace=True)
+#x_train.fillna(x_train.mean(), inplace=True)
 #x_test.fillna(x_test.mean(), inplace=True)
 
 # Verify axes match
@@ -32,8 +58,14 @@ x_train.fillna(x_train.mean(), inplace=True)
 #compare_row_headers(x_train, y_train)
 #compare_row_headers(x_test, y_test)
 
-print(x_train.dtype)
-print(y_train.dtype)
+# T_TEST SAMPLE GROUP TEST
+#y_test_actual = pd.DataFrame(0, index=y_test[['bicalutamide']].index, columns=y_test[['bicalutamide']].columns).values
+#y_test_actual = np.random.randint(2, size=20) # Fill with either 0 or 1 randomly
+#y_test_predicted = np.random.rand(len(y_test_actual))
+#drug_responses_0, drug_responses_1 = get_t_test_groups(y_test_actual, y_test_predicted)
+#System.exit(0)
+
+y_test_prediction = pd.DataFrame(index=y_test.index, columns=y_test.columns)
 
 # Predict the response for each drug individually
 for drug in y_train:
@@ -50,11 +82,16 @@ for drug in y_train:
 
     # Create elastic net model with five-fold cross validation 
     regr = ElasticNetCV(cv=5, random_state=0)
-    regr.fit(x_train_single.values, y_train_single.values)
-    #print(regr.predict(x_test.values))
-    System.exit(0)
+    regr.fit(x_train_single.values, np.ravel(y_train_single.values))
 
-    
+    # Predict the y_test drug response
+    y_test_prediction_single = predict(x_test)
+    #print(y_test_prediction_single)
+    #System.exit(0)
+
+    # Insert drug response vector into y_test matrix
+    #y_test_prediction[[drug]] = y_test_prediction_single
+    #print(y_test_prediction)
 
 # Cross validation: used to select hyperparameters
 #   Do it with diff values of alpha
