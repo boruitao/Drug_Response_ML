e95caf4486a5ed181b28280517b9d761e42208cd
bilasa <bilasa999@gmail.com>
2019-09-25 20:37:27 -0400

    Implement multitask lasso

--- multi_task_predictor.py
+++ multi_task_predictor.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
 from sklearn_pandas import DataFrameMapper
-from sklearn.linear_model import ElasticNetCV
+from sklearn.linear_model import MultiTaskLasso, MultiTaskLassoCV
 from sklearn.datasets import make_regression
 from scipy import stats
 
@@ -46,8 +46,47 @@ def category_to_binary(y_test):
         y_test_binary = y_test_binary.replace("Clinical Progressive Disease", 0)
     return y_test_binary
 
+# Normalizes x_train and x_test per gene
+# Stores result in csv
+def normalize(x_train, x_test):
+    ss = StandardScaler()
+    for gene in x_train:
+        x_train[gene] = ss.fit_transform(x_train[[gene]].values)
+    for gene in x_test:
+        x_test[gene] = ss.fit_transform(x_test[[gene]].values)
+    x_train.T.to_csv(data_path + 'gdsc_expr_postCB(normalized).csv')
+    x_test.T.to_csv(data_path + 'tcga_expr_postCB(normalized).csv')
+
+# Verify that the axes match
+def verify_axes(x_train, y_train, x_test, y_test):
+    compare_column_headers(x_train, x_test)
+    compare_column_headers(y_train, y_test)
+    compare_row_headers(x_train, y_train)
+    compare_row_headers(x_test, y_test)
+
+# Perform one-tailed t-test to verify the mean of resistant group is higher than the mean of sensitive group
+# drug_responses_0: the resistant group
+# drug_responses_1: the sensitive group
+def one_tailed_t_test(drug_responses_0, drug_responses_1):
+    t, p = stats.ttest_ind(drug_responses_0, drug_responses_1)
+
+    # Correct sign
+    if t >= 0:
+        p = p / 2
+    # Incorrect sign
+    else:
+        p = 1 - p / 2
+
+    return t, p
+
 # Path to datasets
 data_path = '../Data/'
+
+# Path to results folder
+results_path = '../Results/'
+
+# Name of model being used
+model_name = 'MultitaskLassoCV'
 
 # Load training and test set
 x_train = pd.read_csv(data_path + 'gdsc_expr_postCB.csv', index_col=0, header=None).T.set_index('cell line id')#.iloc[0:200, 0:200]
@@ -62,90 +101,45 @@ x_train = pd.DataFrame(ss.fit_transform(x_train), index = x_train.index, columns
 y_train = pd.DataFrame(ss.fit_transform(y_train), index = y_train.index, columns = y_train.columns)
 x_test = pd.DataFrame(ss.fit_transform(x_test), index = x_test.index, columns = x_test.columns)
 
-# Verify axes match
-#compare_column_headers(x_train, x_test)
-#compare_column_headers(y_train, y_test)
-#compare_row_headers(x_train, y_train)
-#compare_row_headers(x_test, y_test)
-
-# Matrix to store y_test predictions
-y_test_prediction = pd.DataFrame(index=y_test.index, columns=y_test.columns)
-
 # Matrix to store drug statistics, including t-statistic and p-value for each drug
 results = y_test.describe().T.join(pd.DataFrame(index=y_test.columns, columns=['T-statistic', 'P-value']))
 
-# Predict the response for each drug individually
-for drug in y_train:
+# Drop cell line id in x_train and y_train if one of the drug responses is NaN
+y_train = y_train.dropna() # Drop rows of y_train
+non_null_ids = y_train.index # Get cell line ids that don't have null drug responses
+x_train = x_train[x_train.index.isin(non_null_ids)]
 
-    # Keep only one drug column
-    y_train_single = y_train[[drug]]
+# Create multitask lasso model
+print("Fitting MultiTask Lasso")
+#regr = MultiTaskLasso(alpha=0.0001).fit(x_train, y_train)
+regr = MultiTaskLassoCV(cv=5, random_state=0).fit(x_train, y_train)
+#print(coef_multi_task_lasso_)
 
-    # Drop cell line ids in y_train where drug response is NaN
-    y_train_single = y_train_single.dropna()
-    non_null_ids = y_train_single.index
-    
-    # Drop cell line ids in x_train where drug response is NaN
-    x_train_single = x_train[x_train.index.isin(non_null_ids)]
+####################################################################################################
 
-    # Create elastic net model with five-fold cross validation 
-    print("Fitting ElasticNetCV for drug: " + drug)
-    regr = ElasticNetCV(cv=5, random_state=0)
-    regr.fit(x_train_single.values, np.ravel(y_train_single.values))
+# Predict y_test
+print("Predicting y test...")
+y_test_prediction = pd.DataFrame(data=regr.predict(x_test), index=y_test.index, columns=y_test.columns)
 
-    # Produce prediction vector for y_test drug response
-    print("Predicting y test...")
-    y_test_prediction_single = regr.predict(x_test) # RANDOM NUMBERS FOR TESTING: y_test_prediction_single = np.random.rand(len(y_train[[drug]].values))
+# For each drug, execute a t-test and store the results
+for drug in y_test_binary.columns:
 
-    # Insert prediction vector into matrix of predictions
-    y_test_prediction[drug] = y_test_prediction_single
-
-    # Get the actual y_test binary values
-    y_test_actual_single = y_test_binary[[drug]]
+    # Get the drug response vector for a single drug
+    y_test_prediction_single = y_test_prediction[drug] # assign column headers to y_test_prediction
+    y_test_actual_single = y_test_binary[drug]
 
     # Get sample groups for category 0 and category 1
     drug_responses_0, drug_responses_1 = get_t_test_groups(y_test_actual_single.values, y_test_prediction_single)
-    
+
     # Perform T-test
     print("Performing t-test...")
-    t, p = stats.ttest_ind(drug_responses_0, drug_responses_1)
+    drug_responses_0, drug_responses_1 = get_t_test_groups(y_test_actual_single.values, y_test_prediction_single)
+    t, p = one_tailed_t_test(drug_responses_0, drug_responses_1)
     results.loc[drug, 'T-statistic'] = t
     results.loc[drug, 'P-value'] = p
 
-# Store predictions and results in csv file
-y_test_prediction.to_csv(data_path + 'tcga_dr_prediction(normalized).csv')
-results.to_csv(data_path + 'results.csv')
-
-# Cross validation: used to select hyperparameters
-#   Do it with diff values of alpha
-#   Choose a loss function to choose the alpha that minimizes loss
-#       Ex: euclidean norm, cosine similarity....
-#   Find alpha with "path" --> elasticnet CV, lasso CV
-#   Scikit has modules for cross validation and hyperparam selection
-#   If multiple hyperparams, can have a LOT of possibilites --> scikit learn help save time
-# Lasso
-#   Lasso minimize 1/2 || y - x ||
-#   L1: minimize absolute values of some of weights --> imposes sparsity
-# Elastic
-#   Has extra L2 norm of w
-
-# Normalization of original data
-#   Figure data distribution
-#   
-# Improve code
-#   Pandas, generalizability of data loading
-#   Hyperparameter tuning
-#   Normalization
-#   T-test: scipy
-#   Can report: cross validation accuracy (check for overfitting/underfitting)
-#   Can try nonlinear models: e.g. Support vector regression
-
-# Sensitive: complete response, partial response
-# Resistant: stable disease, progressive disease
-
-# Spend time learning multi-task methods
-
-# Profile compute canada: find what is accessible to us, etc...
-#       Takes time when requesting resources: in case need for next semester
-#       Access GPUs: good for neural networks
-
-# Right things down for report immediately: so that he can give us feedback
+# Store predictions and results in csv files
+prediction_file_name = 'tcga_dr_prediction(' + model_name + ').csv'
+y_test_prediction.to_csv(results_path + prediction_file_name)
+results_file_name = 'results(' + model_name + ').csv'
+results.to_csv(results_path + results_file_name)
