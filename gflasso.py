dfc2191e7b8d569da9bca669d7e23b1c281d8059
bilasa <bilasa999@gmail.com>
2019-11-14 12:22:52 -0500

    Finish up implementation

--- gflasso.py
+++ gflasso.py
@@ -1,10 +1,12 @@
 import numpy as np
+from numpy import linalg as LA
 import pandas as pd
 from sklearn.impute import SimpleImputer
+from sklearn.metrics import mean_squared_error AS mse
 
 
 def absolute_correlation(r_ml):
-        return abs(r_ml)
+    return abs(r_ml)
 
 def squared_correlation(r_ml):
     return r_ml**2
@@ -15,29 +17,32 @@ def thresholded_correlation(r_ml, corr_thresh):
     else:
         return 0
 
-# TO-DO: Borui
+def correlation_function(r_ml, corr_func, corr_thresh):
+    f_r_ml = None
+    if self.corr_func == 'absolute':
+        f_r_ml = self.absolute_correlation(r_ml)
+    elif self.corr_func == 'squared':
+        f_r_ml = self.squared_correlation(r_ml)
+    elif self.corr_func == 'thresholded':
+        f_r_ml = self.thresholded_correlation(r_ml, corr_thresh)
+    else:
+        raise ValueError("Unrecognized correlation function. Please correct to \"absolute\", \"squared\", or \"thresholded\"")
+    return f_r_ml
+
 def mu(J, K, E_size, epsilon):
     D = 1 / 2 * J * (K + E_size)
     mu = epsilon / (2 * D)
     return mu
 
 def C(lambda_, gamma, K, E_size, G, corr_func, corr_thresh):
-    I = np.ones((K, K))
+    I = np.identity(K)
     H = np.zeros((K, E_size))
     e = 0
     for k in range (0, K):
         for m in range(0, K):
             for l in range(0, K):
                 r_ml = G[m][l]
-                f_r_ml = None
-                if self.corr_func == 'absolute':
-                    f_r_ml = self.absolute_correlation(r_ml)
-                elif self.corr_func == 'squared':
-                    f_r_ml = self.squared_correlation(r_ml)
-                elif self.corr_func == 'thresholded':
-                    f_r_ml = self.thresholded_correlation(r_ml, corr_thresh)
-                else:
-                    raise ValueError("Unrecognized correlation function. Please correct to \"absolute\", \"squared\", or \"thresholded\"")
+                f_r_ml = correlation_function(r_ml, corr_func, corr_thresh)
                 if k == m:
                     H[k, e] = f_r_ml
                 elif k == l:
@@ -45,16 +50,27 @@ def C(lambda_, gamma, K, E_size, G, corr_func, corr_thresh):
                 else:
                     H[k, e] = 0
                 e += 1
-    return lambda_ * I, gammma * H
+    return lambda_ * I, gammma * H # Return two matrices: not sure if this is what C is supposed to be
 
-# TO-DO: Borui
-def L_U(X, G, K, mu, lambda_, gamma):
-    return 0
+def L_U(X, G, K, mu, lambda_, gamma, corr_func, corr_thresh):
+    w, v = LA.eig(np.matmul(X.T, X))
+    lambda_max = max(w)
+    d_k_max = 0
+    for k in range(0, K):
+        d_k = 0
+        for m in range(0, K):
+            r_km = G[k][m]
+            f_r_km = correlation_function(r_km, corr_func, corr_thresh)
+            d_k += f_r_km**2
+        if d_k > d_k_max:
+            d_k_max = d_k
+    L_U = lambda_max + (lambda_**2 + 2 * gamma**2 * d_k_max) / mu
+    return L_U
 
 def A_star(W_t, C, mu):
     if mu == 0:
         raise ValueError("Parameter mu cannot be zero, it would cause division by zero.")
-    x = np.matmul(W_t, C) / mu # TO-DO: verify what C is because I don't think it's simply a matrix
+    x = np.matmul(W_t, C) / mu # TO-DO: verify what C is
     if x > -1 and x < -1:
         return x
     elif x >= 1:
@@ -62,29 +78,37 @@ def A_star(W_t, C, mu):
     else:
         return -1
 
-def proximal_gradient_descent(G, X, Y, J, K, lambda_, gamma, epsilon, corr_func, corr_thresh=None):
+def proximal_gradient_descent(G, X, Y, J, K, lambda_, gamma, epsilon, corr_func, corr_thresh=None, iterations=100):
     E_size = K**2
     mu = mu(J, K, E_size, epsilon)
     C = C(K, G)
     L_U = L_U(X, G, K, mu, lambda_, gamma)
     if L_U == 0:
         raise ValueError("L_U cannot be zero, it would cause dividion by zero.")
+    
     W_t = np.zeros((J, K))
     B_t = None
+    cost_history = np.zeros(iterations) # Store sequence of costs at each time t
+    B_t_history = np.zeros(iterations) # Store sequence of weights at each time t (B_t)
     t = 0
-    converged = False
-    while not converged:
+    while t < iterations: # Instead of checking for convergence, do a set amount of iterations
         A_star = A_star(W_t, C, mu)
-        delta_f_tilde = np.add(np.matmul(X.T, np.subtract(np.matmul(X, W_t), Y)), A_star * C))
+        delta_f_tilde = np.add(np.matmul(X.T, np.subtract(np.matmul(X, W_t), Y)), A_star * C)) # TO-DO: verify what C is
         B_t = np.subtract(W_t, delta_f_tilde / L_U)
+        B_t_history[t] = B_t
         if t == 0:
             Z_t = np.zeros(delta_f_tilde.shape)
         else:
             Z_t = np.add(Z_t, (-1 / L_U) * (t + 1) / 2 * delta_f_tilde)
         W_t = np.add((t + 1) / (t + 3) * B_t, 2 / (t + 3) * Z_t)
+        
+        # Calculate cost (MSE)
+        Y_pred = np.matmul(X, W_t)
+        cost = mse(Y, Y_pred)
+        cost_history[t] = cost
+
         t = t + 1
-        # TO-DO: add check to see if we converged, if so set converged=True
-    return B_t
+    return B_t, B_t_history, cost_history
 
 #  ===================== TRAINING SECTION ========================
 print("Retrieving data ....")
