4ec89fcb24d18b090c1bb39238a5ee6c82f6b6af
Borui Tao <borui.tao@mail.mcgill.ca>
2019-11-14 22:08:41 -0500

    fix syntax error

--- gflasso.py
+++ gflasso.py
@@ -2,9 +2,8 @@ import numpy as np
 from numpy import linalg as LA
 import pandas as pd
 from sklearn.impute import SimpleImputer
-from sklearn.metrics import mean_squared_error AS mse
-
-
+from sklearn.metrics import mean_squared_error as mse
+from __future__ import division
 def absolute_correlation(r_ml):
     return abs(r_ml)
 
@@ -19,20 +18,20 @@ def thresholded_correlation(r_ml, corr_thresh):
 
 def correlation_function(r_ml, corr_func, corr_thresh):
     f_r_ml = None
-    if self.corr_func == 'absolute':
-        f_r_ml = self.absolute_correlation(r_ml)
-    elif self.corr_func == 'squared':
-        f_r_ml = self.squared_correlation(r_ml)
-    elif self.corr_func == 'thresholded':
-        f_r_ml = self.thresholded_correlation(r_ml, corr_thresh)
+    if corr_func == 'absolute':
+        f_r_ml = absolute_correlation(r_ml)
+    elif corr_func == 'squared':
+        f_r_ml = squared_correlation(r_ml)
+    elif corr_func == 'thresholded':
+        f_r_ml = thresholded_correlation(r_ml, corr_thresh)
     else:
         raise ValueError("Unrecognized correlation function. Please correct to \"absolute\", \"squared\", or \"thresholded\"")
     return f_r_ml
 
 def mu(J, K, E_size, epsilon):
     D = 1 / 2 * J * (K + E_size)
-    mu = epsilon / (2 * D)
-    return mu
+    mu_result = epsilon / (2 * D)
+    return mu_result
 
 def C(lambda_, gamma, K, E_size, G, corr_func, corr_thresh):
     I = np.identity(K)
@@ -50,12 +49,12 @@ def C(lambda_, gamma, K, E_size, G, corr_func, corr_thresh):
                 else:
                     H[k, e] = 0
                 e += 1
+    return np.concatenate((lambda_ * I, gamma * H), axis=1)
     #return lambda_ * I, gammma * H # Return two matrices: not sure if this is what C is supposed to be
-    return np.concatenate((ambda_ * I, gammma * H), axis=1)
 
 def L_U(X, G, K, mu, lambda_, gamma, corr_func, corr_thresh):
     w, v = LA.eig(np.matmul(X.T, X))
-    lambda_max = max(w)
+    lambda_max = max(w, v)
     d_k_max = 0
     for k in range(0, K):
         d_k = 0
@@ -71,20 +70,21 @@ def L_U(X, G, K, mu, lambda_, gamma, corr_func, corr_thresh):
 def A_star(W_t, C, mu):
     if mu == 0:
         raise ValueError("Parameter mu cannot be zero, it would cause division by zero.")
-    x = np.matmul(W_t, C) / mu # TO-DO: verify what C is
-    if x > -1 and x < -1:
-        return x
-    elif x >= 1:
-        return 1
-    else:
-        return -1
+    inner_matrix = np.matmul(W_t, C) / mu # TO-DO: verify what C is
+    with np.nditer(inner_matrix, op_flags=['readwrite']) as it:
+        for x in it:
+            if x >= 1:
+                x[...] = 1
+            elif x <= -1:
+                x[...] = -1
+    return x
 
 def proximal_gradient_descent(G, X, Y, J, K, lambda_, gamma, epsilon, corr_func, corr_thresh=None, iterations=100):
     E_size = K**2
-    mu = mu(J, K, E_size, epsilon)
-    C = C(K, G)
-    L_U = L_U(X, G, K, mu, lambda_, gamma)
-    if L_U == 0:
+    mu_result = mu(J, K, E_size, epsilon)
+    C_result = C(lambda_, gamma, K, E_size, G, corr_func, corr_thresh)
+    L_U_result = L_U(X, G, K, mu, lambda_, gamma, corr_func, corr_thresh)
+    if L_U_result == 0:
         raise ValueError("L_U cannot be zero, it would cause dividion by zero.")
     
     W_t = np.zeros((J, K))
@@ -93,15 +93,15 @@ def proximal_gradient_descent(G, X, Y, J, K, lambda_, gamma, epsilon, corr_func,
     B_t_history = np.zeros(iterations) # Store sequence of weights at each time t (B_t)
     t = 0
     while t < iterations: # Instead of checking for convergence, do a set amount of iterations
-        A_star = A_star(W_t, C, mu)
+        A_star_res = A_star(W_t, C_result, mu_result)
         #delta_f_tilde = np.add(np.matmul(X.T, np.subtract(np.matmul(X, W_t), Y)), A_star * C)) # TO-DO: verify what C is
-        delta_f_tilde = np.add(np.matmul(X.T, np.subtract(np.matmul(X, W_t), Y)), A_star * np.transpose(C)))
-        B_t = np.subtract(W_t, delta_f_tilde / L_U)
+        delta_f_tilde = np.add(np.matmul(X.T, np.subtract(np.matmul(X, W_t), Y)), np.matmul(A_star_res , np.transpose(C_result)))
+        B_t = np.subtract(W_t, delta_f_tilde / L_U_result)
         B_t_history[t] = B_t
         if t == 0:
             Z_t = np.zeros(delta_f_tilde.shape)
         else:
-            Z_t = np.add(Z_t, (-1 / L_U) * (t + 1) / 2 * delta_f_tilde)
+            Z_t = np.add(Z_t, (-1 / L_U_result) * (t + 1) / 2 * delta_f_tilde)
         W_t = np.add((t + 1) / (t + 3) * B_t, 2 / (t + 3) * Z_t)
         
         # Calculate cost (MSE)
