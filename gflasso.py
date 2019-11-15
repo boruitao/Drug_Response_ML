800c26b55010d08b170d26b5c03899ce1bde19e7
bilasa <bilasa999@gmail.com>
2019-11-14 22:49:19 -0500

    Make fixes and add print statements for testing

--- gflasso.py
+++ gflasso.py
@@ -3,7 +3,10 @@ from numpy import linalg as LA
 import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.metrics import mean_squared_error as mse
-from __future__ import division
+import time
+
+start_time = time.time()
+
 def absolute_correlation(r_ml):
     return abs(r_ml)
 
@@ -29,15 +32,19 @@ def correlation_function(r_ml, corr_func, corr_thresh):
     return f_r_ml
 
 def mu(J, K, E_size, epsilon):
+    print("--- Called mu ---")
+    print("Current time: " + str(time.time() - start_time) + " seconds")
     D = 1 / 2 * J * (K + E_size)
     mu_result = epsilon / (2 * D)
     return mu_result
 
 def C(lambda_, gamma, K, E_size, G, corr_func, corr_thresh):
+    print("--- Called C ---")
+    print("Current time: " + str(time.time() - start_time) + " seconds")
     I = np.identity(K)
     H = np.zeros((K, E_size))
-    e = 0
     for k in range (0, K):
+        e = 0
         for m in range(0, K):
             for l in range(0, K):
                 r_ml = G[m][l]
@@ -53,6 +60,8 @@ def C(lambda_, gamma, K, E_size, G, corr_func, corr_thresh):
     #return lambda_ * I, gammma * H # Return two matrices: not sure if this is what C is supposed to be
 
 def L_U(X, G, K, mu, lambda_, gamma, corr_func, corr_thresh):
+    print("--- Called L_U ---")
+    print("Current time: " + str(time.time() - start_time) + " seconds")
     w, v = LA.eig(np.matmul(X.T, X))
     lambda_max = max(w, v)
     d_k_max = 0
@@ -68,6 +77,8 @@ def L_U(X, G, K, mu, lambda_, gamma, corr_func, corr_thresh):
     return L_U
 
 def A_star(W_t, C, mu):
+    print("--- A_star ---")
+    print("Current time: " + str(time.time() - start_time) + " seconds")
     if mu == 0:
         raise ValueError("Parameter mu cannot be zero, it would cause division by zero.")
     inner_matrix = np.matmul(W_t, C) / mu # TO-DO: verify what C is
@@ -79,7 +90,9 @@ def A_star(W_t, C, mu):
                 x[...] = -1
     return x
 
-def proximal_gradient_descent(G, X, Y, J, K, lambda_, gamma, epsilon, corr_func, corr_thresh=None, iterations=100):
+def proximal_gradient_descent(G, X, Y, J, K, lambda_, gamma, epsilon, iterations, corr_func, corr_thresh=None):
+    print("--- Called proximal_gradient_descent ---")
+    print("Current time: " + str(time.time() - start_time) + " seconds")
     E_size = K**2
     mu_result = mu(J, K, E_size, epsilon)
     C_result = C(lambda_, gamma, K, E_size, G, corr_func, corr_thresh)
@@ -92,19 +105,33 @@ def proximal_gradient_descent(G, X, Y, J, K, lambda_, gamma, epsilon, corr_func,
     cost_history = np.zeros(iterations) # Store sequence of costs at each time t
     B_t_history = np.zeros(iterations) # Store sequence of weights at each time t (B_t)
     t = 0
+    print("--- Beginning optimization ---")
+    print("Current time: " + str(time.time() - start_time) + " seconds")
     while t < iterations: # Instead of checking for convergence, do a set amount of iterations
+        print("--- Beginning iteration " + str(t) + "---")
+        print("Current time: " + str(time.time() - start_time) + " seconds")
         A_star_res = A_star(W_t, C_result, mu_result)
         #delta_f_tilde = np.add(np.matmul(X.T, np.subtract(np.matmul(X, W_t), Y)), A_star * C)) # TO-DO: verify what C is
+        print("--- Calculating delta_f_tilde for iteration " + str(t) + "---")
+        print("Current time: " + str(time.time() - start_time) + " seconds")
         delta_f_tilde = np.add(np.matmul(X.T, np.subtract(np.matmul(X, W_t), Y)), np.matmul(A_star_res , np.transpose(C_result)))
+        print("--- Calculating B_t " + str(t) + "---")
+        print("Current time: " + str(time.time() - start_time) + " seconds")
         B_t = np.subtract(W_t, delta_f_tilde / L_U_result)
         B_t_history[t] = B_t
+        print("--- Calculating Z_t for iteration " + str(t) + "---")
+        print("Current time: " + str(time.time() - start_time) + " seconds")
         if t == 0:
             Z_t = np.zeros(delta_f_tilde.shape)
         else:
             Z_t = np.add(Z_t, (-1 / L_U_result) * (t + 1) / 2 * delta_f_tilde)
+        print("--- Calculating W_t for iteration " + str(t) + "---")
+        print("Current time: " + str(time.time() - start_time) + " seconds")
         W_t = np.add((t + 1) / (t + 3) * B_t, 2 / (t + 3) * Z_t)
         
         # Calculate cost (MSE)
+        print("--- Calculating MSE for iteration " + str(t) + "---")
+        print("Current time: " + str(time.time() - start_time) + " seconds")
         Y_pred = np.matmul(X, W_t)
         cost = mse(Y, Y_pred)
         cost_history[t] = cost
@@ -116,12 +143,12 @@ def proximal_gradient_descent(G, X, Y, J, K, lambda_, gamma, epsilon, corr_func,
 print("Retrieving data ....")
 data_path = '../Data/'
 results_path = '../Results/'
-drug_cids = pd.read_csv(data_path + 'drug_drug_similarity.csv',index_col=0, header=None, low_memory=False).T.set_index('drug').apply(pd.to_numeric)
+drug_names = pd.read_csv(data_path + 'drug_drug_similarity.csv',index_col=0, header=None, low_memory=False).T.set_index('drug').apply(pd.to_numeric)
 train_x = pd.read_csv(data_path + 'gdsc_expr_postCB(normalized).csv', index_col=0, header=None, low_memory=False).T.set_index('cell line id').apply(pd.to_numeric)#.iloc[0:10:,0:5]
 train_y = pd.read_csv(data_path + 'gdsc_dr_lnIC50.csv', index_col=0, header=None, low_memory=False).T.set_index('cell line id').apply(pd.to_numeric)#.iloc[0:10:,]
 
 #select 8 drugs which only exists in the drug-drug similarity matrix
-train_y = train_y.filter(drug_cids)
+train_y = train_y.filter(drug_names)
 
 # If a drug's DR is NaN, set it to the mean of the cell lines' DR for that drug
 imp = SimpleImputer(missing_values=np.nan, strategy='mean')
@@ -131,3 +158,8 @@ correlation_matrix = drug_cids.values
 X = train_x.values
 Y = train_y.values
 print("Data ready to train")
+
+B_t, B_t_history, cost_history = proximal_gradient_descent(
+    G=correlation_matrix, X=X, Y=Y, J=np.size(X, 1), K=np.size(Y, 1), 
+    lambda_=1, gamma=1, epsilon=1, corr_func='absolute', iterations=100
+)
